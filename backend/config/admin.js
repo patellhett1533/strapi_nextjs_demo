7fadff168019892417e54f5f965ba222c406b12b
het patel <hppatidar12d@gmail.com>
2024-10-05 17:06:29 +0530

    update a configuration

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,7 +1,5 @@
 module.exports = ({ env }) => ({
-  url: `${env("BACKEND_PUBLIC_URL", "http://127.0.0.1:1337")}/admin`,
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  url: `${env("PUBLIC_URL", "http://127.0.0.1:1337")}/admin`,
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
   },
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,8 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("BACKEND_PUBLIC_URL", "http://localhost:1337"),
-  proxy: env.bool("PROXY", true),
   app: {
     keys: env.array("APP_KEYS"),
   },
