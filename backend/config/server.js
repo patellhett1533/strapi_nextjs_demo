b0f5316891a241e71cd0c47072e2cb7a2866fa93
het patel <hppatidar12d@gmail.com>
2024-10-05 14:02:04 +0530

    update a admin configuration

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,6 +2,12 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: "https://strapi-nextjs-demo.onrender.com",
+  admin: {
+    url: env(
+      "ADMIN_BACKEND_URL",
+      "https://strapi-nextjs-demo.onrender.com/admin"
+    ),
+  },
   app: {
     keys: env.array("APP_KEYS"),
   },
