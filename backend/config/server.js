759072703cf0de7693e504e8e511ec8ff8e123a4
het patel <hppatidar12d@gmail.com>
2024-10-05 14:56:41 +0530

    update a postgresql config

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,13 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://strapi-nextjs-demo.onrender.com",
-  admin: {
-    url: env(
-      "ADMIN_BACKEND_URL",
-      "https://strapi-nextjs-demo.onrender.com/admin"
-    ),
-  },
   app: {
     keys: env.array("APP_KEYS"),
   },
@@ -15,3 +8,4 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
+``;
